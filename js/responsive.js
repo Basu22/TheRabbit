7340979ac82c71253908d3a2a16d91519ef9631a
Basilio Ossvald <basilio.ossval@gmail.com>
2023-05-12 18:32:05 -0300

    primer subida

---
+++ js/responsive.js
@@ -0,0 +1,9 @@
+export function resize(){
+    window.addEventListener('resize',()=>{
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        console.log(windowWidth)
+    })
+}
+
+
